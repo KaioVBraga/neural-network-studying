d6b321f4c50884cec62e3ea970a8d8a8e7a33ffe
kaiovbraga <kaiovbraga2001@gmail.com>
2020-08-20 23:04:21 -0300

    Letting size of layers be based on biases length

---
+++ better-layer.py
@@ -0,0 +1,49 @@
+import random
+
+def neuron(inputs, weights, bias):
+  weigthed_list = list(inputs[i] * weights[i] for i, x in enumerate(inputs))
+  return sum(weigthed_list) + bias
+
+def create_outputs(inputs, weights, biases):
+  middle_outputs = []
+  for i in range(0, len(biases)):
+    middle_outputs.append(neuron(inputs, weights, biases[i]))
+  return middle_outputs
+
+def create_layers(inputs, weights_list, biases_list):
+  layers = list()
+  layers.append(create_outputs(inputs, weights_list[0], biases_list[0]))
+
+  if(len(weights_list) > 2):
+    for i in range(1, len(weights_list)-1):
+      layers.append(create_outputs(layers[i-1], weights_list[i], biases_list[i]))
+
+  layers.append(create_outputs(layers[-1], weights_list[-1], biases_list[-1]))
+
+  return layers
+
+def main():
+  inputs = [1,2,3,4,5,6]
+
+  weights_list = [
+    [1,2,3,4,5,6],
+    [1,2,1,2,3],
+    [1,2,1,2,3],
+    [1,2,1,2,3]
+  ]
+
+  biases_list = [
+    [1,2,3,4,5],
+    [1.33,2.5,1.96,2,3],
+    [1,2,4,2,3],
+    [1,2]
+  ]
+
+  layers = create_layers(inputs, weights_list, biases_list)
+
+  print("0 : ", inputs)
+  for i, layer in enumerate(layers):
+    print(i+1,": ",layer)
+
+
+main()
--- simple-layer.py
+++ simple-layer.py
@@ -3,15 +3,28 @@ import random
 inputs = [1, 2, 3, 4, 5, 6]
 # weights = [i for i, x in enumerate(inputs)]
 weights = [1,2,3,4,5,6]
+weights1 = [1,2,1,2,3]
 
 def neuron(inputs, weights):
   bias = round(random.random()*100)/10
   weigthed_list = list(inputs[i] * weights[i] for i, x in enumerate(inputs))
   return sum(weigthed_list) + bias
 
+def create_middle_outputs(inputs, weights, size):
+  middle_outputs = []
+  for i in range(0, size):
+    middle_outputs.append(neuron(inputs, weights))
+  return middle_outputs
 
-outputs = []
+
+middle_outputs = create_middle_outputs(inputs, weights, len(weights1))
+
+
+final_outputs = []
 for i in range(0, 2):
-  outputs.append(neuron(inputs, weights))
+  final_outputs.append(neuron(middle_outputs, weights1))
 
-print(outputs)
+
+print(inputs)
+print(middle_outputs)
+print(final_outputs)
